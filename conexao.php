19e3cca08d529994cdf3ecf67a5dd418145fb9e4
ElivanSpiecker <104541379+ElivanSpiecker@users.noreply.github.com>
2025-03-27 21:18:18 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "127.0.0.1:3306";  // Use apenas "localhost" ou "127.0.0.1"
 $username = "root";         // Seu usuário MySQL
-$password = "123";  // Substitua pela sua senha (deixe em branco se não houver senha)
+$password = "";  // Substitua pela sua senha (deixe em branco se não houver senha)
 $dbname = "meu_banco";          // Nome do banco de dados
 
 // Cria a conexão
