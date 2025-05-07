b44d6f8a2839bb76040747d2aa53aac834a36202
ElivanSpiecker <104541379+ElivanSpiecker@users.noreply.github.com>
2025-05-07 20:14:24 -0300

    Update email.php

--- email.php
+++ email.php
@@ -8,14 +8,14 @@ function enviarEmail($to, $assunto, $mensagem) {
     $mail = new PHPMailer(true);
     try {
         $mail->isSMTP();
-        $mail->Host = 'smtp.seuservidor.com';
+        $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'seuemail@exemplo.com';
-        $mail->Password = 'sua_senha';
+        $mail->Username = 'eli.spiecker@gmail.com';
+        $mail->Password = 'jpzo emxl dwxd xtnc';
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
-        $mail->setFrom('seuemail@exemplo.com', 'Sistema de Tarefas');
+        $mail->setFrom('eli.spiecker@gmail.com', 'Sistema de Tarefas');
         $mail->addAddress($to);
         $mail->Subject = $assunto;
         $mail->Body    = $mensagem;
