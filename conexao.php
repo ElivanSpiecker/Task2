ba73d1d737057c18dd1198cea9575da8f209f493
ElivanSpiecker <elivan.spiecker@universo.univates.br>
2025-07-06 17:47:45 -0300

    pipeline

--- conexao.php
+++ conexao.php
@@ -1,10 +1,18 @@
 <?php
-$servername = "127.0.0.1:3306";  // Use apenas "localhost" ou "127.0.0.1"
-$username = "root";         // Seu usuário MySQL
-$password = "123";  // Substitua pela sua senha (deixe em branco se não houver senha)
-$dbname = "meu_banco";          // Nome do banco de dados
+// Lê o arquivo .env na mesma pasta
+$env = parse_ini_file(__DIR__ . '/.env');
+
+// Parâmetros de conexão
+$servername = "127.0.0.1";
+$username = "root";
+$password = "123";
+$dbname = $env['DB_NAME']; // Vem do .env
 
 // Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Verifica conexão
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
+}
 ?>
