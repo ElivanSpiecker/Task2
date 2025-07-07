2765d352cdcf85aa6ad5b5bd1d9cee6b4779747d
ElivanSpiecker <elivan.spiecker@universo.univates.br>
2025-07-06 21:20:06 -0300

    testeFinal

---
+++ db/migrations/V2teste.sql
@@ -0,0 +1,4 @@
+CREATE TABLE teste (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(255) NOT NULL
+);
--- listar_tarefa.php
+++ listar_tarefa.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listagem de Tarefas</title>
+    <title>Listagem de Tarefas:</title>
     <style>
         table {
             width: 100%;
